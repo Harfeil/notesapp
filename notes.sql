0fdf4093c91d655df48b5d7eaa0bb012d5818ba1
ChonaSabanal <sabanalchona@gmail.com>
2024-09-20 14:11:44 +0800

    updated

--- notes.sql
+++ notes.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 20, 2024 at 01:57 AM
--- Server version: 11.5.2-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Sep 20, 2024 at 08:08 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,21 +28,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `note_tbl` (
+  `note_id` int(100) NOT NULL,
   `note_name` varchar(100) NOT NULL,
   `note_date` date NOT NULL,
   `note_message` varchar(100) NOT NULL,
   `note_status` varchar(100) NOT NULL,
-  `user_id` int(100) NOT NULL,
-  `note_id` int(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
-
---
--- Dumping data for table `note_tbl`
---
-
-INSERT INTO `note_tbl` (`note_name`, `note_date`, `note_message`, `note_status`, `user_id`, `note_id`) VALUES
-('asdff', '2024-09-19', 'asd', 'New', 1, 1),
-('asd', '2024-09-19', 'asd', 'New', 1, 2);
+  `user_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -51,21 +43,14 @@ INSERT INTO `note_tbl` (`note_name`, `note_date`, `note_message`, `note_status`,
 --
 
 CREATE TABLE `users` (
+  `user_id` int(100) NOT NULL,
   `user_fname` varchar(100) NOT NULL,
   `user_lname` varchar(100) NOT NULL,
   `user_age` int(100) NOT NULL,
   `user_gender` varchar(100) NOT NULL,
   `user_email` varchar(100) NOT NULL,
-  `user_password` varchar(100) NOT NULL,
-  `user_id` int(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`user_fname`, `user_lname`, `user_age`, `user_gender`, `user_email`, `user_password`, `user_id`) VALUES
-('harfeil', 'Salmeron', 12, 'Male ', 'harfeilsalmeron1@gmail.com', '$2y$10$2uBRElRxMJ6q5UpgXzAb8.hI9SjY6ap9sLcOyFf8Kt.m3IJRFqqkK', 1);
+  `user_password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Indexes for dumped tables
@@ -92,13 +77,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `note_tbl`
 --
 ALTER TABLE `note_tbl`
-  MODIFY `note_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `note_id` int(100) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(100) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
